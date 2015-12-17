f233c41a1647a405a5b148ba19a5d9db8018c117
1009qugeorge <1009qugeorge@PC8817.domepfc.net>
2015-12-17 21:58:47 +0100

    new info 2

--- cinemaPHP/index.php
+++ cinemaPHP/index.php
@@ -16,6 +16,9 @@ and open the template in the editor.
         <br>
         <a href="affichage.php"> afficher table </a>
         
+        <?php 
+            echo "";
+        ?>
         
     </body>
 </html>
